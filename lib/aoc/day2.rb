d33c8156c4509387a8517e5ad0503d2c93c05b1f
Julian Doherty (madlep) <madlep@madlep.com>
2016-12-15 17:38:31 +1100

    implement part 2

--- lib/aoc/day2.rb
+++ lib/aoc/day2.rb
@@ -27,6 +27,19 @@ class AOC::Day2
       .join
   end
 
+  def run_part2(instructions)
+    current_button = Button["5"]
+    Parser
+      .new(instructions)
+      .map{|line|
+        line.inject(current_button){|button, movement|
+          current_button = next_button_part2(button, movement)
+        }
+      }
+      .map(&:value)
+      .join
+  end
+
   def next_button_part1(button, movement)
     match [button, movement],
       on([Button["1"], Up],     button),
@@ -73,6 +86,75 @@ class AOC::Day2
       on([Button["9"], Down],   button),
       on([Button["9"], Left],   Button["8"]),
       on([Button["9"], Right],  button)
+  end
+
+  def next_button_part2(button, movement)
+    match [button, movement],
+      on([Button["1"], Up], button),
+      on([Button["1"], Down], Button["3"]),
+      on([Button["1"], Left], button),
+      on([Button["1"], Right], button),
+
+      on([Button["2"], Up], button),
+      on([Button["2"], Down], Button["6"]),
+      on([Button["2"], Left], button),
+      on([Button["2"], Right], Button["3"]),
+
+      on([Button["3"], Up], Button["1"]),
+      on([Button["3"], Down], Button["7"]),
+      on([Button["3"], Left], Button["2"]),
+      on([Button["3"], Right], Button["4"]),
+
+      on([Button["4"], Up], button),
+      on([Button["4"], Down], Button["8"]),
+      on([Button["4"], Left], Button["3"]),
+      on([Button["4"], Right], button),
+
+
+      on([Button["5"], Up], button),
+      on([Button["5"], Down], button),
+      on([Button["5"], Left], button),
+      on([Button["5"], Right], Button["6"]),
+
+      on([Button["6"], Up], Button["2"]),
+      on([Button["6"], Down], Button["A"]),
+      on([Button["6"], Left], Button["5"]),
+      on([Button["6"], Right], Button["7"]),
+
+      on([Button["7"], Up], Button["3"]),
+      on([Button["7"], Down], Button["B"]),
+      on([Button["7"], Left], Button["6"]),
+      on([Button["7"], Right], Button["8"]),
+
+      on([Button["8"], Up], Button["4"]),
+      on([Button["8"], Down], Button["C"]),
+      on([Button["8"], Left], Button["7"]),
+      on([Button["8"], Right], Button["9"]),
+
+      on([Button["9"], Up], button),
+      on([Button["9"], Down], button),
+      on([Button["9"], Left], Button["8"]),
+      on([Button["9"], Right], button),
+
+      on([Button["A"], Up], Button["6"]),
+      on([Button["A"], Down], button),
+      on([Button["A"], Left], button),
+      on([Button["A"], Right], Button["B"]),
+
+      on([Button["B"], Up], Button["7"]),
+      on([Button["B"], Down], Button["D"]),
+      on([Button["B"], Left], Button["A"]),
+      on([Button["B"], Right], Button["C"]),
+
+      on([Button["C"], Up], Button["8"]),
+      on([Button["C"], Down], button),
+      on([Button["C"], Left], Button["B"]),
+      on([Button["C"], Right], button),
+
+      on([Button["D"], Up], Button["B"]),
+      on([Button["D"], Down], button),
+      on([Button["D"], Left], button),
+      on([Button["D"], Right], button)
   end
 
   class Parser
