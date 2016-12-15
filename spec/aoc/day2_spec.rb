7f84e53b0abbea8bb7c16609caa37de4dba06fb5
Julian Doherty (madlep) <madlep@madlep.com>
2016-12-15 16:54:12 +1100

    failing test for part2

--- spec/aoc/day2_spec.rb
+++ spec/aoc/day2_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe AOC::Day2 do
       instructions = "ULL\nRRDDD\nLURDL\nUUUUD"
       expect(subject.run_part1(instructions)).to eq("1985")
     end
+
+    it "finds part 2 code correctly" do
+      instructions = "ULL\nRRDDD\nLURDL\nUUUUD"
+      expect(subject.run_part2(instructions)).to eq("5DB3")
+    end
   end
 end
