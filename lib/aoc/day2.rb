7e6faa28b086ea1cc511b238839e81410aef088c
Julian Doherty (madlep) <madlep@madlep.com>
2016-12-15 17:38:04 +1100

    make Button values be String not Integer
    to allow A-D to be used as Button values too

--- lib/aoc/day2.rb
+++ lib/aoc/day2.rb
@@ -5,7 +5,7 @@ require "hamster"
 class AOC::Day2
   include Algebrick::Matching
 
-  Button = Algebrick.type { fields Integer }
+  Button = Algebrick.type { fields String }
 
   Movement = Algebrick.type {
     variants  Up = atom,
@@ -15,7 +15,7 @@ class AOC::Day2
   }
 
   def run_part1(instructions)
-    current_button = Button[5]
+    current_button = Button["5"]
     Parser
       .new(instructions)
       .map{|line|
@@ -29,50 +29,50 @@ class AOC::Day2
 
   def next_button_part1(button, movement)
     match [button, movement],
-      on([Button[1], Up],     button),
-      on([Button[1], Down],   Button[4]),
-      on([Button[1], Left],   button),
-      on([Button[1], Right],  Button[2]),
+      on([Button["1"], Up],     button),
+      on([Button["1"], Down],   Button["4"]),
+      on([Button["1"], Left],   button),
+      on([Button["1"], Right],  Button["2"]),
 
-      on([Button[2], Up],     button),
-      on([Button[2], Down],   Button[5]),
-      on([Button[2], Left],   Button[1]),
-      on([Button[2], Right],  Button[3]),
+      on([Button["2"], Up],     button),
+      on([Button["2"], Down],   Button["5"]),
+      on([Button["2"], Left],   Button["1"]),
+      on([Button["2"], Right],  Button["3"]),
 
-      on([Button[3], Up],     button),
-      on([Button[3], Down],   Button[6]),
-      on([Button[3], Left],   Button[2]),
-      on([Button[3], Right],  button),
+      on([Button["3"], Up],     button),
+      on([Button["3"], Down],   Button["6"]),
+      on([Button["3"], Left],   Button["2"]),
+      on([Button["3"], Right],  button),
 
-      on([Button[4], Up],     Button[1]),
-      on([Button[4], Down],   Button[7]),
-      on([Button[4], Left],   button),
-      on([Button[4], Right],  Button[5]),
+      on([Button["4"], Up],     Button["1"]),
+      on([Button["4"], Down],   Button["7"]),
+      on([Button["4"], Left],   button),
+      on([Button["4"], Right],  Button["5"]),
 
-      on([Button[5], Up],     Button[2]),
-      on([Button[5], Down],   Button[8]),
-      on([Button[5], Left],   Button[4]),
-      on([Button[5], Right],  Button[6]),
+      on([Button["5"], Up],     Button["2"]),
+      on([Button["5"], Down],   Button["8"]),
+      on([Button["5"], Left],   Button["4"]),
+      on([Button["5"], Right],  Button["6"]),
 
-      on([Button[6], Up],     Button[3]),
-      on([Button[6], Down],   Button[9]),
-      on([Button[6], Left],   Button[5]),
-      on([Button[6], Right],  button),
+      on([Button["6"], Up],     Button["3"]),
+      on([Button["6"], Down],   Button["9"]),
+      on([Button["6"], Left],   Button["5"]),
+      on([Button["6"], Right],  button),
 
-      on([Button[7], Up],     Button[4]),
-      on([Button[7], Down],   button),
-      on([Button[7], Left],   button),
-      on([Button[7], Right],  Button[8]),
+      on([Button["7"], Up],     Button["4"]),
+      on([Button["7"], Down],   button),
+      on([Button["7"], Left],   button),
+      on([Button["7"], Right],  Button["8"]),
 
-      on([Button[8], Up],     Button[5]),
-      on([Button[8], Down],   button),
-      on([Button[8], Left],   Button[7]),
-      on([Button[8], Right],  Button[9]),
+      on([Button["8"], Up],     Button["5"]),
+      on([Button["8"], Down],   button),
+      on([Button["8"], Left],   Button["7"]),
+      on([Button["8"], Right],  Button["9"]),
 
-      on([Button[9], Up],     Button[6]),
-      on([Button[9], Down],   button),
-      on([Button[9], Left],   Button[8]),
-      on([Button[9], Right],  button)
+      on([Button["9"], Up],     Button["6"]),
+      on([Button["9"], Down],   button),
+      on([Button["9"], Left],   Button["8"]),
+      on([Button["9"], Right],  button)
   end
 
   class Parser
